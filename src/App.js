7718c3a4b7ae296490bef4cd9fe2299e2e8dfda3
StevenJBurns <stevenjburns@msn.com>
2018-05-29 04:17:10 -0600

    More CSS antics; semantic renaming of common tags; meta tags added to public index.html

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header>
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Stagecoach Meat Co.</h1>
+          <h4>Wiggins, Colorado</h4>
+          <h4>970-483-7280</h4>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <nav>
+          <ul>
+            <li><a href="products.html">The Menu</a></li>
+            <li><a href="services.html">Services</a></li>
+            <li><a href="faq.html">FAQ</a></li>
+            <li><a href="contact.html">Contact Us</a></li>
+            <li><a href="about.html">About Us</a></li>
+          </ul>
+        </nav>
+        <main>
+          <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+        </main>
+        <footer>
+          <h4>&copy;2018 &bull; Stagecoach Meat Co., LLC</h4>
+        </footer>
       </div>
     );
   }
