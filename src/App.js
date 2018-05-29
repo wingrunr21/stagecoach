d67f897c1817c9c4f3d6b7febefe70f6bf7eaada
StevenJBurns <stevenjburns@msn.com>
2018-05-29 16:38:02 -0600

    Functional React Router, yay

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
+/* React and React Router imports */
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
 
-/* import page components */
+/* Asset imports */
+import './App.css';
+import logo from './logo.svg';
+
+/* page components import*/
 import PageHome from './pages/PageHome';
-// import PageProducts from './pages/PageProducts';
-// import PageServices from './pages/PageServices';
-// import PageContact from './pages/PageContact';
-// import PageAbout from './pages/PageAbout';
-// import PageFAQ from './pages/PageFAQ';
+import PageProducts from './pages/PageProducts';
+import PageServices from './pages/PageServices';
+import PageContact from './pages/PageContact';
+import PageAbout from './pages/PageAbout';
+import PageFAQ from './pages/PageFAQ';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Stagecoach Meat Co.</h1>
-          <h4>Wiggins, Colorado</h4>
-          <h4>970-483-7280</h4>
-        </header>
-        <nav>
-          <ul>
-            <li><a href="products.html">The Menu</a></li>
-            <li><a href="services.html">Services</a></li>
-            <li><a href="faq.html">FAQ</a></li>
-            <li><a href="contact.html">Contact Us</a></li>
-            <li><a href="about.html">About Us</a></li>
-          </ul>
-        </nav>
-        <main>
-          <PageHome />
-          <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
-        </main>
-        <footer>
-          <h4>&copy;2018 &bull; Stagecoach Meat Co., LLC</h4>
-        </footer>
-      </div>
+      <Router>
+        <div className="App">
+          <header>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Stagecoach Meat Co.</h1>
+            <h4>Wiggins, Colorado</h4>
+            <h4>970-483-7280</h4>
+          </header>
+          <nav>
+            <ul>
+              <li><Link to="/products">The Menu</Link></li>
+              <li><Link to="/services">Services</Link></li>
+              <li><Link to="/faq">FAQ</Link></li>
+              <li><Link to="/contact">Contact Us</Link></li>
+              <li><Link to="/about">About Us</Link></li>
+            </ul>
+          </nav>
+          <main>
+            <Route path="/" exact component={PageHome} />
+            <Route path="/products" component={PageProducts} />
+            <Route path="/services" component={PageServices} />
+            <Route path="/FAQ" component={PageFAQ} />
+            <Route path="/contact" component={PageContact} />
+            <Route path="/about" component={PageAbout} />
+            <p className="App-intro">To get started, edit <code>src/App.js</code> and save to reload.</p>
+          </main>
+          <footer>
+            <h4>&copy;2018 &bull; Stagecoach Meat Co., LLC</h4>
+          </footer>
+        </div>
+      </Router>
     );
   }
 }
