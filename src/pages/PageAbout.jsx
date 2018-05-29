5f60cfdfa2795a2ec962716e3cc038fe0fbd364d
StevenJBurns <stevenjburns@msn.com>
2018-05-29 16:04:00 -0600

    Added basic render for FAQ, Services, Contact and About pages; still need a Router

---
+++ src/pages/PageAbout.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageAbout = () => {
+  return (
+    <h1>About</h1>
+  )
+}
+
+export default PageAbout;
---
+++ src/pages/PageServices.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PageServices = () => {
+  return (
+    <h1>Services</h1>
+  )
+}
+
+export default PageServices;
