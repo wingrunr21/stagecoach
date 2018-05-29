f0bf3dff07db4f7f5f00e07507794a81ea6ddcf5
StevenJBurns <stevenjburns@msn.com>
2018-05-29 17:53:12 -0600

    Created an AppHeader but it isnt used yet

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AppHeader = () => {
+  return(
+    <header>
+      <h1 className="App-title">Stagecoach Meat Co.</h1>
+      <h4>Wiggins, Colorado</h4>
+      <h4>970-483-7280</h4>
+    </header>
+  )
+};
+
+export default AppHeader;
