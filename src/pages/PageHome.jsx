f7eaea6b29d1675fe0fe9232196f99acfff98249
StevenJBurns <stevenjburns@msn.com>
2018-08-06 20:04:55 -0600

    Experimenting with video background images

--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import VideoSunset from "../assets/sunset.mp4"
 
 const PageHome = () => {
   return (
-    <h1>Main</h1>
+    <div>
+      <h1>Main</h1>
+      <video id="videoSunset" autoPlay muted loop style={{overflowY: 'hidden'}}>
+        <source src={VideoSunset} type="video/mp4" />
+      </video>
+    </div>
   )
 }
 
