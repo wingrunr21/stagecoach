b51bc7e84f63f03aa0be354e94ba3b57ca50509e
SJB <StevenJBurns@users.noreply.github.com>
2018-08-09 23:52:17 -0600

    Update PageFAQ.jsx

--- src/pages/PageFAQ.jsx
+++ src/pages/PageFAQ.jsx
@@ -2,7 +2,46 @@ import React from "react";
 
 const PageFAQ = () => {
   return (
-    <h1>FAQ</h1>
+     <div class="Content RoundedTop">
+      <h1>FAQ</h1>
+      <h2 style="font-weight: bolder; text-align: center; text-decoration: underline">Frequently Asked Questions</h2>
+        <dl>
+          <dt>I’d like to cook for my family or friends.  What serving size do you recommend per person?</dt>
+          <dd>As much as you can eat</dd>
+          <dt>How are beef aged?  How long are they aged?</dt>
+          <dd>Like a fine wine</dd>
+          <dt>Do you age hogs the same as beef?</dt>
+          <dd>Yes...</dd>
+          <dt>When can I expect my order to be ready?</dt>
+          <dd>When we get to it</dd>
+          <dt>How soon can I get an animal in?</dt>
+          <dd>How soon can you get here?</dd>
+          <dt>Where is Stagecoach Meat Co. located?  How do I find you?</dt>
+          <dd>Google Maps</dd>
+          <dt>Do you accept checks?</dt>
+          <dd>If it wont bounce</dd>
+          <dt>What cuts do I have to choose from?</dt>
+          <dd>All of the above</dd>
+          <dt>What are your charges for slaughtering and processing?</dt>
+          <dd>The slaughter charge is a flat charge.  Processing charges are based on the dress weight of the animal.  Please call for current rates.</dd>
+          <dt>What does “dress weight” mean?</dt>
+          <dd>Dress weight refers to the carcass hanging weight after it has been butchered and skinned.  Expect an approximately 30% loss from the live weight of the animal.</dd>
+          <dt>How do I cook certain cuts of meat?</dt>
+          <dd>Microwave or stove</dd>
+          <dt>Do I need to bring boxes or coolers to take the meat home?</dt>
+          <dd>BYOB</dd>
+          <dt>When should I drop the animal off?</dt>
+          <dd>ASAP</dd>
+          <dt>How soon after I’m notified that my order is ready should I pick it up?</dt>
+          <dd>Our freezer space is valuable.  Please plan to pick your order up within a week after notification.</dd>
+          <dt>I’m new to custom meat processing.  Do you have cutting instruction forms available?</dt>
+          <dd>Were working on it</dd>
+          <dt>Do you charge extra for making hamburger patties or sausage patties.</dt>
+          <dd>No. Only for special sauce...</dd>
+          <dt>How do you package the cuts of meat?</dt>
+          <dd>All meat is wrapped first in a layer of plastic meat film and then covered with freezer paper.</dd>
+      </dl>
+    </div>
   )
 }
 
