f8c39d6104df78e1c57e5eac87754bf54b3c1816
StevenJBurns <stevenjburns@msn.com>
2018-08-18 19:32:22 -0600

    App pages and router functionality refactored out to AppMain component successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 /* React and React Router imports */
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom'; // Link, Switch, Redirect
+import { BrowserRouter as Router } from 'react-router-dom'; // Link, Switch, Redirect
+
+/* App Components Imports */
+import { AppHeader } from "./components/AppHeader.jsx";
+import { AppNav } from "./components/AppNav.jsx";
+import { AppMain } from "./components/AppMain.jsx";
+import { AppFooter } from "./components/AppFooter.jsx";
 
 /* Style Imports */
 import './styles/App.css';
@@ -8,40 +14,18 @@ import './styles/AppHeader.css';
 import './styles/AppNav.css';
 import './styles/AppFooter.css';
 
-/* Page Components Import*/
-import PageHome from './pages/PageHome';
-import PageProducts from './pages/PageProducts';
-import PageServices from './pages/PageServices';
-import { PageContact } from './pages/PageContact';
-import { PageAbout } from './pages/PageAbout';
-import { PageFAQ } from './pages/PageFAQ';
 
-import { AppHeader } from "./components/AppHeader.jsx";
-import { AppNav } from "./components/AppNav.jsx";
-import { AppMain } from "./components/AppMain.jsx";
-import { AppFooter } from "./components/AppFooter.jsx";
-
-
-class App extends React.Component {
+export class App extends React.Component {
   render() {
     return (
       <Router basename={process.env.PUBLIC_URL}>
         <div className="app">
           <AppHeader />
           <AppNav />
-          <main>
-            <Route path="/" exact component={PageHome} />
-            <Route path="/products" component={PageProducts} />
-            <Route path="/services" component={PageServices} />
-            <Route path="/FAQ" component={PageFAQ} />
-            <Route path="/contact" component={PageContact} />
-            <Route path="/about" component={PageAbout} />
-          </main>
+          <AppMain />
           <AppFooter />
         </div>
       </Router>
     );
   };
 };
-
-export default App;
--- src/components/AppMain.jsx
+++ src/components/AppMain.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import {Route} from "react-router-dom";
+
+/* App Page Imports */
+import PageHome from '../pages/PageHome';
+import PageProducts from '../pages/PageProducts';
+import PageServices from '../pages/PageServices';
+import { PageContact } from '../pages/PageContact';
+import { PageAbout } from '../pages/PageAbout';
+import { PageFAQ } from '../pages/PageFAQ';
+
 
 export const AppMain = (props) => {
   return (
     <main>
-
-
+      <Route path="/" exact component={PageHome} />
+      <Route path="/products" component={PageProducts} />
+      <Route path="/services" component={PageServices} />
+      <Route path="/FAQ" component={PageFAQ} />
+      <Route path="/contact" component={PageContact} />
+      <Route path="/about" component={PageAbout} />
     </main>
   );
 }
