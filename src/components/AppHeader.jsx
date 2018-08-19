0fe5905785bfd9f02a5bf127333326b5b37d2ab5
StevenJBurns <stevenjburns@msn.com>
2018-08-18 18:48:32 -0600

    CSS flex style changes for AppHeader

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -4,8 +4,10 @@ export const AppHeader = (props) => {
   return(
     <header id="app-header">
       <h1 id="app-title">Stagecoach Meat Co.</h1>
-      <h4>Wiggins, Colorado</h4>
-      <h4>970-483-7280</h4>
+      <div id="app-info">
+        <h4>Wiggins, Colorado</h4>
+        <h4>970-483-7280</h4>
+      </div>
     </header>
   );
 };
