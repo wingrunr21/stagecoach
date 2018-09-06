38261b2e026c68f0e06afac6e4d617c1ab905c81
StevenJBurns <stevenjburns@msn.com>
2018-09-05 19:10:52 -0600

    Finally updating PageFAQ with real answers -- my snark has been present since 2011

--- src/pages/PageFAQ.jsx
+++ src/pages/PageFAQ.jsx
@@ -12,44 +12,49 @@ export const PageFAQ = (props) => {
       <ul id="ul-faq-list">
         <li className="li-faq-item">
           <span className="span-faq-q">I’d like to cook for my family or friends.  What serving size do you recommend per person?</span>
-          <span className="span-faq-a">As much as you can eat.</span>
+          {/* <span className="span-faq-a">As much as you can eat.</span> */}
+          <span className="span-faq-a">We recommend 8 to 12oz per adult</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">How are beef aged?  How long are they aged?</span>
-          <span className="span-faq-a">Like a fine wine.</span>
+          {/* <span className="span-faq-a">Like a fine wine.</span> */}
+          <span className="span-faq-a">The beef are hanged in a temperature controlled cooler for up to 3 weeks, or your preference</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">Do you age hogs the same as beef?</span>
-          <span className="span-faq-a">Yes... Next question.</span>
+          {/* <span className="span-faq-a">Yes... Next question.</span> */}
+          <span className="span-faq-a">Hogs also hang in temperature controlled cooler for up to a week</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">When can I expect my order to be ready?</span>
-          <span className="span-faq-a">When we get to it</span>
+          {/* <span className="span-faq-a">When we get to it</span> */}
+          <span className="span-faq-a">You can expect your order to be ready 48 hours after it has been cut. We will notify you by telephone when it is ready to be picked up</span>
         </li>
-        <li className="li-faq-item">
+        {/* <li className="li-faq-item">
           <span className="span-faq-q">How soon can I get an animal in?</span>
           <span className="span-faq-a">How soon can you get here?</span>
-        </li>
+        </li> */}
         <li className="li-faq-item">
           <span className="span-faq-q">Where is Stagecoach Meat Co. located?  How do I find you?</span>
-          <span className="span-faq-a">Google Maps</span>
+          <span className="span-faq-a">Our location is on the Contact Page. Google maps shows our location incorrectly</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">Do you accept checks?</span>
-          <span className="span-faq-a">If it wont bounce</span>
+          {/* <span className="span-faq-a">If it wont bounce</span> */}
+          <span className="span-faq-a">Yes. We accept checks, cash and credit cards.</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">What cuts do I have to choose from?</span>
-          <span className="span-faq-a">All of the above</span>
+          <span className="span-faq-a">You will be offered a cut sheet (either pork or beef), and you can choose your cuts on that form. If you have any questions when filling it out, we invite you to contact us, and we'll be more than happy to help you.</span>
         </li>
         <li className="li-faq-item">
           <span className="span-faq-q">What are your charges for slaughtering and processing?</span>
           <span className="span-faq-a">The slaughter charge is a flat charge.  Processing charges are based on the dress weight of the animal.  Please call for current rates.</span>
         </li>
-        <li className="li-faq-item">
+        {/* <li className="li-faq-item">
           <span className="span-faq-q">What does “dress weight” mean?</span>
           <span className="span-faq-a">Dress weight refers to the carcass hanging weight after it has been butchered and skinned.  Expect an approximately 30% loss from the live weight of the animal.</span>
-        </li>
+        </li> */}
         <li className="li-faq-item">
           <span className="span-faq-q">How do I cook certain cuts of meat?</span>
           <span className="span-faq-a">Microwave or stove</span>
