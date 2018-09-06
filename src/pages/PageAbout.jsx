1a0590ce18aabf98517a579337c03f6eda5f844d
StevenJBurns <stevenjburns@msn.com>
2018-09-05 18:43:50 -0600

    Added some more conent to PageAbout

--- src/pages/PageAbout.jsx
+++ src/pages/PageAbout.jsx
@@ -11,7 +11,8 @@ export const PageAbout = (props) => {
     <div id="div-page-wrapper-about" className="div-page-wrapper">
       <section>
         <h2>About Stagecoach Meat Co LLC</h2>
-        <p>Stagecoach Meat Company, LLC is a family-owned and operated custom processing and retail meat sales company, located in Wiggins, Colorado. Although the Company opened for business in March 2006, the owner and manager of the Company has over 20 years of experience in the meat business. Our processing plant in Wiggins also includes a retail store with a full line of high quality, USDA-inspected beef and pork products.</p>
+        <p>Stagecoach Meat Company, LLC is a family-owned and operated custom processing and retail meat sales company, located in Wiggins, Colorado. Although the company opened for business in March 2006, the owner and manager of the company has over 20 years of experience in the meat business. Our processing plant in Wiggins also includes a retail store with a full line of high quality, USDA-inspected beef and pork products.</p>
+        <p>The founder, Ray Von Loh, his wife, children, and a host of dedicated employees oversee day-to-day operations. Our number one goal is customer satisfaction, quality products, and service.</p>
       </section>
       <section>
         <h2>Ray Von Loh</h2>
