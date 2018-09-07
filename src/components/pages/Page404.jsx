134e2069c52d68e637fa67230b503542044b8043
StevenJBurns <stevenjburns@msn.com>
2018-09-07 17:26:51 -0600

    Started broken grid experiment with Page404 component... looks OK -- needs some work but it's feasible and asthetic so far

--- src/components/pages/Page404.jsx
+++ src/components/pages/Page404.jsx
@@ -4,9 +4,13 @@ import React from "react";
 /* Stylesheets */
 import "../../styles/pages/Page404.css";
 
+/* Assets */
+import CowSkull from "../../assets/images/cow-skull.jpg";
+
 
 export const Page404 = () => (
   <main className="main-404">
-    <h2>You're Lost...</h2>
+    <img id="img-cow-skull" src={CowSkull} alt="404"></img>
+    <h4>You gambled and you lost...</h4>
   </main>
 );
