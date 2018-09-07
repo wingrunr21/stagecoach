76cae3b605cdc85c75492723023c4d8d3751d1dd
StevenJBurns <stevenjburns@msn.com>
2018-09-07 17:56:51 -0600

    Adding broken grid styling to PageHome.... looks OK so far, needs work

--- src/components/pages/PageHome.jsx
+++ src/components/pages/PageHome.jsx
@@ -4,11 +4,14 @@ import React from "react";
 /* Stylesheets */
 import "../../styles/pages/PageHome.css";
 
+/* Assets */
+import CornField from "../../assets/images/corn-field.jpg";
+
 
 export const PageHome = () => (
   <main class="main-home">
+    <img id="img-corn-field" src={CornField} alt="Corn Field"></img>
     <section>
-      <h1>Main</h1>
       <p>Welcome to Stagecoach Meat Co.</p>
     </section>
   </main>
