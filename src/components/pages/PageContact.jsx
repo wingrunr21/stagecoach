0691e042d89af562e4e44bf98c4de99c349c4ca8
StevenJBurns <stevenjburns@msn.com>
2018-09-10 15:08:32 -0600

    Google Maps component installed via NPM; works OK with decent styling so far but requires an API key... working on that now

--- src/components/pages/PageContact.jsx
+++ src/components/pages/PageContact.jsx
@@ -1,11 +1,18 @@
 /* React */
 import React from "react";
 
+/* Compoenents */
+import GoogleMapReact from "google-map-react";
+
 /* Stylesheets */
 import "../../styles/pages/PageContact.css";
 
 
 export const PageContact = (props) => {
+  const styleGoogleMap = { height: 288 + 'px', width: 90 + '%', minWidth: 320 + 'px', maxWidth: 640 +'px', margin: "24px auto", display: 'inlineBlock' }; 
+  const defaultMapCenter= { lat: 40.2304149, lng: -104.0814956 };
+  const defaultMapZoom = 16;
+
   return (
     <main className="main-contact">
       <p>Our processing plant and shop is located in Wiggins, CO. Google Maps will incorrectly plot our location.  See the image below for the correct location.</p>
@@ -13,12 +20,15 @@ export const PageContact = (props) => {
       <ul className="ul-contact">
         <li>Shop Location</li>
         <li>600 W 3rd Ave</li>
-        <li>Colorado 80654</li> 
+        <li>WIggins, CO 80654</li> 
       </ul>
+      <div style={styleGoogleMap}>
+        <GoogleMapReact defaultCenter={ defaultMapCenter} defaultZoom={ defaultMapZoom } />
+      </div>
       <ul className="ul-contact">
         <li>Mailing Address</li>
         <li>PO Box 413</li>
-        <li>Wiggins, Colorado 80654</li>
+        <li>Wiggins, CO 80654</li>
       </ul>
       <ul className="ul-contact">
         <li>Phone and Fax</li>
