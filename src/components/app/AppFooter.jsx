8a7825aea1b02144c29ddff6678588dd64b57fae
StevenJBurns <stevenjburns@msn.com>
2018-09-10 13:35:38 -0600

    Added Font Awsome icon for Facebook to AppFooter; needs a little styling

--- src/components/app/AppFooter.jsx
+++ src/components/app/AppFooter.jsx
@@ -4,9 +4,16 @@ import React from "react";
 /* Stylesheets */
 import '../../styles/app/AppFooter.css';
 
+/* Font Awesome! */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
 
 export const AppFooter = () => (
   <footer id="app-footer">
     <h5>&copy;2018 &bull; Stagecoach Meat Co., LLC</h5>
+    <a href="https://github.com/StevenJBurns/extrasolar">
+      <FontAwesomeIcon className="fa-logo" icon={ faFacebook } size="2x" />
+    </a>
   </footer>
 );
