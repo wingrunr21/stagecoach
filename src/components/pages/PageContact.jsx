35d79376da6e65731c0986f5307059ce20fd3c98
StevenJBurns <stevenjburns@msn.com>
2018-09-10 16:43:30 -0600

    More content and styling for PageContact... coming along better needs a little more work

--- src/components/pages/PageContact.jsx
+++ src/components/pages/PageContact.jsx
@@ -18,30 +18,55 @@ export const PageContact = (props) => {
 
   return (
     <main className="main-contact">
-      <p>Our processing plant and shop is located in Wiggins, CO. Google Maps will <em>occasionally</em> plot our location incorrectly.  See the image below for the correct location.</p>
-      <h1>Contact</h1>
-      <ul className="ul-contact">
-        <li>Shop Location</li>
-        <li>600 W 3rd Ave</li>
-        <li>WIggins, CO 80654</li> 
-      </ul>
-      <div style={styleGoogleMap}>
-        <GoogleMapReact defaultCenter={ defaultMapCenter} defaultZoom={ defaultMapZoom } bootstrapURLKeys={keyAPI}>
-          <MapLabel lat={ defaultMapCenter.lat } lng={ defaultMapCenter.lng } text={ "Stagecoach Meat Co." }/>
-        </GoogleMapReact>
-      </div>
-      <ul className="ul-contact">
-        <li>Mailing Address</li>
-        <li>PO Box 413</li>
-        <li>Wiggins, CO 80654</li>
-      </ul>
-      <ul className="ul-contact">
-        <li>Phone and Fax</li>
-        <li>Phone : 970-483-7280</li>
-        <li>Fax : 970-483-8180</li>
-      </ul>
-      <p>To contact us via email: <a href="mailto:svonloh@yahoo.com?subject=Attention%20Sean">Stagecoach Meat</a></p>
-      <p>Also check out our Facebook page...</p>
+      <section id="section-intro">
+        <p>Our processing plant and shop is located in Wiggins, CO.</p>
+      </section>
+      <section id="section-business-hours">
+        <h3>Business Hours</h3>
+        <hr />
+      </section>
+      <section id="section-phone-and-fax">
+        <h3>Phone and Fax</h3>
+        <hr />
+        <ul className="ul-contact">
+          <li>Phone and Fax</li>
+          <li>Phone : 970-483-7280</li>
+          <li>Fax : 970-483-8180</li>
+        </ul>
+      </section>
+      <section id="section-map">
+        <h3>Processing Plant and Retail Shop</h3>
+        <hr />
+        <p>Be aware that Google Maps will occasionally plot our location incorrectly.  If you end up in the wrong place plase call us!</p>
+        <ul className="ul-contact">
+          <li>Shop Location</li>
+          <li>600 W 3rd Ave</li>
+          <li>WIggins, CO 80654</li> 
+        </ul>
+        <div style={styleGoogleMap}>
+          <GoogleMapReact defaultCenter={ defaultMapCenter} defaultZoom={ defaultMapZoom } bootstrapURLKeys={keyAPI}>
+            <MapLabel lat={ defaultMapCenter.lat } lng={ defaultMapCenter.lng } text={ "Stagecoach Meat Co." }/>
+          </GoogleMapReact>
+        </div>
+      </section>
+      <section id="section-mailing-address">
+      <h3>Mailing Address</h3>
+      <hr />
+      <p>Should you need to send physical mail, our mail is delivered to this address:</p>
+        <ul className="ul-contact">
+          <li>Mailing Address</li>
+          <li>PO Box 413</li>
+          <li>Wiggins, CO 80654</li>
+        </ul>
+      </section>
+      <section id="section-email">
+        <p>To contact us via email: <a href="mailto:svonloh@yahoo.com?subject=Attention%20Sean">Stagecoach Meat</a></p>
+      </section>
+      <section id="section-feacebook">
+        <h3>Social Media</h3>
+        <hr />
+        <p>Also check out our Facebook page!</p>
+      </section>
     </main>
   );
 };
