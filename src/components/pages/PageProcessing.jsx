ecc845037577ebb349ca785206fffc4b8c544589
StevenJBurns <stevenjburns@msn.com>
2018-09-13 23:39:18 -0600

    Added initial bolierplate content from Brooke for PageProcessing; added basic consistent CSS as well

--- src/components/pages/PageProcessing.jsx
+++ src/components/pages/PageProcessing.jsx
@@ -8,5 +8,6 @@ import "../../styles/pages/PageProcessing.css";
 export const PageProcessing = () => (
   <main className="main-processing">
     <h1>Processing</h1>
+    <p>Because we are a small family-owned plant, we offer high quality processing, cutting beef to the customer's specifications. We're available to answer any questions you may have along the way. The slaughter charge for beef is $75 per head, and the cost of processing is $0.81/lb. After cutting, the beef is then double-wrapped and put into the freezer.</p>
   </main>
 );
