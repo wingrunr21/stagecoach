681653681ec1786ecef39a2c85d1444a6fb2dcf0
StevenJBurns <stevenjburns@msn.com>
2018-09-14 18:46:33 -0600

    Successfully imported old stagecoach.png into AppHeader -- needs a lot of love on position an size though

--- src/components/app/AppHeader.jsx
+++ src/components/app/AppHeader.jsx
@@ -10,6 +10,7 @@ import '../../styles/app/AppHeader.css';
 
 export const AppHeader = () => (
   <header id="app-header">
+    <div id="div-stageoach-png" />
     <h2 id="app-header-h2">Stagecoach Meat Co.</h2>
     <h5 id="app-header-h5">Wiggins, Colorado &bull; 970-483-7280</h5>
     <div id="div-empty" />
