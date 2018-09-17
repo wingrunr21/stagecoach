3b77adc862e15ded85cd2d8131e14f513b928658
StevenJBurns <stevenjburns@msn.com>
2018-09-17 17:54:51 -0600

    Added the actual beef cut sheet from Brooke -- its curved

--- src/components/pages/PageProcessing.jsx
+++ src/components/pages/PageProcessing.jsx
@@ -6,7 +6,7 @@ import "../../styles/pages/PageProcessing.css";
 
 /* Assets */
 import BarbedWire from "../../assets/images/barbed-wire.jpg";
-// import CutsheetBeefPDF from "../../assets/svg/cutsheet-beef.svg";
+import CutsheetBeefPDF from "../../assets/documents/stagecoach-cut-sheet-beef.pdf";
 // import CutsheetPorkPDF from "../../assets/svg/cutsheet-pork.svg";
 import ResumeTestPDF from "../../assets/documents/steven-j-burns-resume.pdf";
 
@@ -18,10 +18,11 @@ export const PageProcessing = () => (
     <p>The cut sheets we use to process your order are linked below.  If you have the ability to fill them out in advance they can be faxed to 970-483-8180 or sent via email to svonloh@yahoo.com. If you don't have the option to print and return the cut sheet to us, we have paper sheets available at our shop location.</p>
     <h5>Cut sheets in PDF format</h5>
     <div id="div-button-wrapper">
-      <input type="button" className="input-cutsheet" value="Beef Cut Sheet"></input>
+      {/* <input type="button" className="input-cutsheet" value="Beef Cut Sheet"></input>
       <input type="button" className="input-cutsheet" value="Pork Cut Sheet"></input>
-      <input type="button" className="input-cutsheet" value="Steve - Resume"></input>
-      <a href={ResumeTestPDF}>TEST</a>
+      <input type="button" className="input-cutsheet" value="Steve - Resume"></input> */}
+      <a className="a-pdf-link" href={CutsheetBeefPDF}>BEEF</a>
+      <a className="a-pdf-link" href={ResumeTestPDF}>TEST</a>
     </div>
     <img id="img-barbed-wire" src={BarbedWire} alt="Barbed Wire Flourish"></img>
 
