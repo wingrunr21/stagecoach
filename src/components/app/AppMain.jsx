53e10183e2434f982010bad5ccd1eb3df97d2075
StevenJBurns <stevenjburns@msn.com>
2018-11-11 23:56:11 -0700

    Fixed indenting in AppMain

--- src/components/app/AppMain.jsx
+++ src/components/app/AppMain.jsx
@@ -18,18 +18,18 @@ import "../../styles/app/AppMain.css";
 const AppMain = ({ location }) => {  
   return (
     <React.Fragment>
-    <TransitionGroup component={null} appear>
-      <CSSTransition key={location.key} classNames={'fade'} timeout={{enter: 500, exit: 500}}>
-        <Switch location={location}>
-          <Route exact path="/" component={PageHome} />
-          <Route exact path="/processing" component={PageProcessing} />
-          <Route exact path="/retail" component={PageRetail} />
-          <Route exact path="/faq" component={PageFAQ} />
-          <Route exact path="/contact" component={PageContact} />
-          <Route component={Page404} />
-        </Switch>
-      </CSSTransition>
-    </TransitionGroup>
+      <TransitionGroup component={null} appear>
+        <CSSTransition key={location.key} classNames={'fade'} timeout={{enter: 500, exit: 500}}>
+          <Switch location={location}>
+            <Route exact path="/" component={PageHome} />
+            <Route exact path="/processing" component={PageProcessing} />
+            <Route exact path="/retail" component={PageRetail} />
+            <Route exact path="/faq" component={PageFAQ} />
+            <Route exact path="/contact" component={PageContact} />
+            <Route component={Page404} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
     </React.Fragment>
   )
 };
