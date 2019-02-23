0369669a907ad04901537be17a4845c7012decd1
StevenJBurns <stevenjburns@msn.com>
2019-02-22 19:02:25 -0700

    create index.js for individual page component imports and exports

---
+++ src/components/pages/index.js
@@ -0,0 +1,15 @@
+import PageHome from './PageHome';
+import PageRetail from './PageRetail';
+import PageProcessing from './PageProcessing';
+import PageContact from './PageContact';
+import PageFAQ from './PageFAQ';
+import Page404 from './Page404';
+
+export {
+  PageHome,
+  PageProcessing,
+  PageRetail,
+  PageFAQ,
+  PageContact,
+  Page404
+};
