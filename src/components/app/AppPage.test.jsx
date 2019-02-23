67551671c8339177532cb420d26077afd6039503
StevenJBurns <stevenjburns@msn.com>
2019-02-23 02:00:32 -0700

    Create successful test in AppPage.test.js

--- src/components/app/AppPage.test.jsx
+++ src/components/app/AppPage.test.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { mount, shallow } from 'enzyme';
-import { AppPage} from './AppPage';
-import { PageHome, Page404 } from '../pages'
+import AppPage from './AppPage';
+import { PageHome } from '../pages'
 
 
 describe('AppPage', () => {
-  it('props.page should render an empty page without a page prop', () => {
+  it('props.page should render Page404 without a page prop', () => {
     const wrapper = mount(<AppPage />)
     expect(wrapper).toBeDefined();
   });
