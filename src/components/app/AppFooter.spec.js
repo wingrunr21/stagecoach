65a6a6287b9b545386a252233df69cc5ec5c3c0c
StevenJBurns <stevenjburns@msn.com>
2019-02-26 02:46:31 -0700

    Create basic passing tests for AppHeader and AppFooter

---
+++ src/components/app/AppFooter.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AppFooter } from './';
+
+describe('AppHAppFootereader', () => {
+  it('should render an HTML5 footer', () => {
+    const wrapper = shallow(<AppFooter />)
+    expect(wrapper).toBeDefined();
+  });
+});
---
+++ src/components/app/AppHeader.spec.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AppHeader } from './';
+
+describe('AppHeader', () => {
+  it('should render an HTML5 header', () => {
+    const wrapper = shallow(<AppHeader />)
+    expect(wrapper).toBeDefined();
+  });
+});
