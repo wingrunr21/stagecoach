351b33a792171478c3a476ac9ee45c28c2f794a9
StevenJBurns <stevenjburns@msn.com>
2019-03-10 17:15:27 -0600

    Move AppMain component, spec and SCSS into their own folder

--- src/components/app/AppMain/AppMain.spec.js
+++ src/components/app/AppMain/AppMain.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { AppMain } from './';
+import { AppMain } from '..';
 import { BrowserRouter } from 'react-router-dom';
 
 describe('AppHeader', () => {
