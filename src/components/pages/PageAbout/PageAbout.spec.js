a4171fcbe1d65c0983ae671aeace38fb50213ecd
StevenJBurns <stevenjburns@msn.com>
2019-03-10 17:19:20 -0600

    Move PageAbout component, spec and SCSS into their own folder

--- src/components/pages/PageAbout/PageAbout.spec.js
+++ src/components/pages/PageAbout/PageAbout.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { PageAbout } from './';
+import { PageAbout } from '..';
 
 describe('PageAbout', () => {
   it('should render the About page', () => {
