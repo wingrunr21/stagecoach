3425a23dd535ab1635b88813e190b375d1ab1102
StevenJBurns <stevenjburns@msn.com>
2019-03-10 17:18:35 -0600

    Move Page404 component, spec and SCSS into their own folder

--- src/components/pages/Page404/Page404.spec.js
+++ src/components/pages/Page404/Page404.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 // import { BrowserRouter } from 'react-router-dom';
-import { Page404 } from './';
+import { Page404 } from '..';
 
 describe('AppHeader', () => {
   it('should render the 404 page', () => {
