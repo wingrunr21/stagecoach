89d7296c3a50af29ab1320be387427768e9bd8b9
StevenJBurns <stevenjburns@msn.com>
2019-03-10 17:19:56 -0600

    Move PageContact component, spec and SCSS into their own folder

--- src/components/pages/PageContact/PageContact.spec.js
+++ src/components/pages/PageContact/PageContact.spec.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { PageContact } from './';
+import { PageContact } from '..';
 
 describe('PageContact', () => {
   it('should render the Contact page', () => {
