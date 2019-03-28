e1119727c5faabcf30d0ac7e2c267cca7db1f16e
StevenJBurns <stevenjburns@msn.com>
2019-03-28 17:49:31 -0600

    Fix AppFooter title in describe block

--- src/components/app/AppFooter/AppFooter.spec.js
+++ src/components/app/AppFooter/AppFooter.spec.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { AppFooter } from '..';
 
-describe('AppHAppFootereader', () => {
+describe('AppFooter', () => {
   it('should render an HTML5 footer', () => {
     const wrapper = shallow(<AppFooter />)
     expect(wrapper).toBeDefined();
