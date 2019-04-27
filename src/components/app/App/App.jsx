b883ed79ed22255972c9fcb54435b7574f0c413e
StevenJBurns <stevenjburns@msn.com>
2019-04-27 17:03:30 -0600

    add SCSS import to App.jsx

--- src/components/app/App/App.jsx
+++ src/components/app/App/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { AppMain, AppHeader, AppFooter, AppNav } from '..';
+import './App.scss';
 
 const App = props => (
   <React.Fragment>
