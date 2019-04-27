f876effa8a05907b41412dc3f402d772f18cc1dc
StevenJBurns <stevenjburns@msn.com>
2019-04-27 17:06:09 -0600

    create index.js for ui level components

---
+++ src/components/ui/index.js
@@ -0,0 +1,9 @@
+import { PorkCutsSVG } from './PorkCutsSVG';
+import { BeefCutsSVG } from './BeefCutsSVG';
+import { CutSheetContainer } from './CutSheetContainer';
+
+export {
+  PorkCutsSVG,
+  BeefCutsSVG,
+  CutSheetContainer,
+};
