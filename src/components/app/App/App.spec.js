b515960e32ddbb3ed3c26f139436ab2a797b22d3
StevenJBurns <stevenjburns@msn.com>
2019-04-27 17:04:29 -0600

    Add MemoryRouter for r-t-l tests

--- src/components/app/App/App.spec.js
+++ src/components/app/App/App.spec.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { render, cleanup } from 'react-testing-library';
-import { App } from '..';
+import { MemoryRouter } from 'react-router-dom';
+import App from '.';
 
 describe('App', () => {
   afterEach(cleanup);
 
   it('should render the App container', () => {
-    const { container } = render(<App />);
+    const { container } = render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>);
     expect(container).toBeDefined();
   });
 });
