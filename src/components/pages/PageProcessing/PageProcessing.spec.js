c8cd06a33ebf54e7fd474f37196d20bebc52b115
StevenJBurns <stevenjburns@msn.com>
2019-04-27 17:08:22 -0600

    add r-t-l cleanup method to PageProcessing spec

--- src/components/pages/PageProcessing/PageProcessing.spec.js
+++ src/components/pages/PageProcessing/PageProcessing.spec.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { reander, cleanup } from 'react-testing-library';
+import { render, cleanup } from 'react-testing-library';
 import { PageProcessing } from '..';
 
 describe('PageProcessing', () => {
+  afterEach(cleanup);
+  
   it('should render the FAQ page', () => {
     const { container } = render(<PageProcessing />);
     expect(container).toBeDefined();
