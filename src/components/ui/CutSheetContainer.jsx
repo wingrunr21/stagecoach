30b1af79ac21314c006501282b3b68d7a8d42f44
StevenJBurns <stevenjburns@msn.com>
2019-04-27 17:08:50 -0600

    clean up new CutSheetContainer component

--- src/components/ui/CutSheetContainer.jsx
+++ src/components/ui/CutSheetContainer.jsx
@@ -1,46 +1,32 @@
-/* React */
 import React from "react";
-
-/* Stylesheets */
-import "./CutSheetContainer.css";
-
-/* Components */
+import "./CutSheetContainer.scss";
 // import { BeefCutsSVG } from "./BeefCutsSVG.jsx";
 // import { PorkCutsSVG } from "./PorkCutsSVG.jsx";
 
+export const CutSheetContainer = props => {
+  const { data } = props;
 
-export class CutSheetContainer extends React.Component {
-  constructor(props) {
-    super(props);
+  const [cutsheetData, setCutsheetData] = React.useState(props.data);
+  const [selectedCut, setSelectedCut] = React.useState(null);
 
-    this.state = {
-      cuts: props.data,
-      selectedSection: null
-    }
-  }
+  React.useEffect(() => { setCutsheetData(data); }, []);
 
-  changeSection = (selectedSection) => {
-    this.setState({selectedSection: selectedSection});
-    console.log(selectedSection);
-  }
-
-  render() {
-
-    return (
-      <div id="div-svg-container">
-        { this.props.children }
-        <div className="div-cut-list-container">
-          <h4>{this.props.title} Cuts</h4>
-          <hr style={{width: "288px"}}></hr>
-          <ul className="ul-cut-list">
-            { 
-            this.state.selectedSection ? 
-            this.props.data[this.state.selectedSection].map((item, index) => <li key={index}>{item}</li> ) :
-            <h5>Select a section on the chart</h5>
-            }
-          </ul>
-        </div>
+  const changeSection = nextCut => setSelectedCut(nextCut);
+  
+  return (
+    <div id="div-svg-container">
+      { props.children }
+      <div className="div-cut-list-container">
+        <h4>{props.title} Cuts</h4>
+        <hr style={{width: '18rem'}}></hr>
+        <ul className="ul-cut-list">
+          { 
+          selectedCut ? 
+          props.data[selectedCut].map((item, index) => <li key={index}>{item}</li> ) :
+          <h5>Select a section on the chart</h5>
+          }
+        </ul>
       </div>
-    )
-  }
+    </div>
+  );
 }
