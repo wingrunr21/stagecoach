c189eae86d6c830eb87fe91a08b2188459099396
StevenJBurns <stevenjburns@msn.com>
2019-04-29 19:14:56 -0600

    refactor: use dynamic year in AppFooter copyright

--- src/components/app/AppFooter/AppFooter.jsx
+++ src/components/app/AppFooter/AppFooter.jsx
@@ -4,15 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import './AppFooter.scss';
 
-const AppFooter = () => (
-  <footer id="app-footer">
-    <a href="https://www.facebook.com/Stagecoach-Meat-Company-LLC-205877879464025/">
-      <FontAwesomeIcon className="fa-logo" icon={ faFacebook } size="2x" />
-    </a>
-    <h5>600 W 3rd Ave &bull; Wiggins, CO 80654</h5>
-    <h5>&copy;2018 &bull; Stagecoach Meat Co., LLC</h5>
-  </footer>
-);
+const AppFooter = () => {
+  const year = new Date().getFullYear();
+  
+  return (
+    <footer id="app-footer">
+      <a href="https://www.facebook.com/Stagecoach-Meat-Company-LLC-205877879464025/">
+        <FontAwesomeIcon className="fa-logo" icon={ faFacebook } size="2x" />
+      </a>
+      <h5>600 W 3rd Ave &bull; Wiggins, CO 80654</h5>
+      <h5>&copy;{year} &bull; Stagecoach Meat Co., LLC</h5>
+    </footer>
+  );
+};
 
 AppFooter.propTypes = {
   children: PropTypes.element.isRequired,
