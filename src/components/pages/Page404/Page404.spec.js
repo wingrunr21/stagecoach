1dc1478513d9ead22ec0f73fb6c40bd7715330b4
StevenJBurns <stevenjburns@msn.com>
2019-04-29 19:17:21 -0600

    test: add afterEach(cleanup) to Page404 spec

--- src/components/pages/Page404/Page404.spec.js
+++ src/components/pages/Page404/Page404.spec.js
@@ -3,6 +3,8 @@ import { render, cleanup } from 'react-testing-library';
 import { Page404 } from '..';
 
 describe('<Page404 />', () => {
+  afterEach(cleanup);
+  
   it('should render the 404 page', () => {
     const { container } = render(<Page404 />);
     expect(container).toBeDefined();
